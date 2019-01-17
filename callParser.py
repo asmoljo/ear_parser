e9b10d3f6a809e6312e9fabe6df529d4fe113183
Antonio Smoljo <antonio.smoljo@vestigo.hr>
2019-01-17 16:28:00 +0100

    kreirane dodatne funkcije u textFileUtilities, callParser i postojece funkcije doradjene

--- callParser.py
+++ callParser.py
@@ -12,6 +12,7 @@ from modules import textFileUtilities, jarFileUtilities
 
 hfs_home = os.environ['HFS_HOME']
 template_dir = hfs_home+'/bin/ear_parser/templates/'
+properties_build_file_path = hfs_home+'/config/properties.build'
 
 def help():
     print '\nSkripta se mora pokrenuti u HFS_HOME folderu!'
@@ -24,7 +25,7 @@ def help():
 def main():
     try:
         if sys.argv[2] == 'zaba':
-            #textFileUtilities.test1()
+
             textFileUtilities.recreate_tmp_directory('tmp')
             textFileUtilities.change_working_directory('tmp')
 
@@ -37,27 +38,26 @@ def main():
             textFileUtilities.search_and_replace_in_file('META-INF/application.xml', 'META-INF/application.xml', '>/dc<', '>cash<')
 
             if not textFileUtilities.search_in_file('WEB-INF/web.xml','security-role'):
-                textFileUtilities.add_in_file_from_template('META-INF/application.xml',os.path.join(template_dir, 'template_security_role.txt'),'</application>','before')
-                textFileUtilities.add_in_file_from_template('WEB-INF/web.xml',os.path.join(template_dir, 'template_security_role_and_constrain.txt'),'</web-app>', 'before')
+                textFileUtilities.add_text_block_to_file_from_template('META-INF/application.xml', os.path.join(template_dir, 'template_security_role.txt'), '</application>', 'before')
+                textFileUtilities.add_text_block_to_file_from_template('WEB-INF/web.xml', os.path.join(template_dir, 'template_security_role_and_constrain.txt'), '</web-app>', 'before')
 
-
-
-
-
-
+            textFileUtilities.add_text_block_to_file_from_template_with_parameters('META-INF/ejb-jar.xml', os.path.join(template_dir, 'template_resource_ref_ejb3.txt'), properties_build_file_path, ['RES_REF_NAME_CONTROLLER_DATASOURCE'], '<display-name>spray-controller-ejb</display-name>', 'after' )
 
         elif sys.argv[2] == 'cg':
             print 'CG parse'
+
+        elif sys.argv[2] == 'test':
+            print textFileUtilities.add_text_block_to_file_from_template_with_parameters('META-INF/ejb-jar.xml', os.path.join(template_dir, 'template_resource_ref_ejb3.txt'), properties_build_file_path, ['RES_REF_NAME_CONTROLLER_DATASOURCE'])
 
         else:
             print 'Ne postoji aplikacija: %s' % sys.argv[2]
 
     except IndexError, e:
-        print 'Index greska: %s' % e
+        print '\nIndex greska: %s' % e
         help()
         return False
     except Exception, e:
-        print 'Greska: %s' % e
+        print '\nGreska: %s' % e
         help()
         return False
 
--- modules/textFileUtilities.py
+++ modules/textFileUtilities.py
@@ -68,26 +68,90 @@ def search_and_replace_in_file(file_to_modify, file_to_save, search_string, repl
         return False
 
 
-def add_in_file_from_template(file_to_add,template_file,starting_line,position):
+def add_text_block_to_file_from_template(file_to_add, template_file_path, starting_line, position):
     try:
-        f = open(template_file,'r').read()
+        f_template = open(template_file_path, 'r').read()
+        text_block = f_template.rstrip()
         for line in fileinput.input(file_to_add, inplace=1):
-            if line.startswith(starting_line):
+            if starting_line in line:
                 if position == 'after':
-                    print line,
-                    print f.rstrip()
+                    print text_block
                 elif position == 'before':
-                    print f.rstrip()
-                    print line,
+                    print text_block
                 else:
                     print 'Pozicija teksta koji se dodaje u file nije ispravno zadana. Moze biti "after" ili "before"!'
                     return False
+            print line, #zarez znaci da se ne ispisuje nova prazna linija poslije komande 'print'(sto python inace radi)
 
-        print line,
         print 'Text dodan u datoteku %s.' % file_to_add
         return True
 
     except Exception, e:
         print e
         return False
+
+
+def add_text_block_to_file_from_template_with_parameters(file_to_add, template_file_path, properties_file_path, parameter_names_list, starting_line, position):
+    parameters_dict = read_properties_file_parameters(parameter_names_list, properties_file_path)
+    text_block = get_text_block_from_template_with_parameters(parameters_dict, template_file_path)
+    try:
+        for line in fileinput.input(file_to_add, inplace=1):
+            if starting_line in line:
+                if position == 'after':
+                    print line,
+                    print text_block
+                elif position == 'before':
+                    print text_block
+                    print line,
+                else:
+                    print text_block #Ako nije ispravno zadan 'position', onda se text_block dodaje iznad zadane linije 'starting_line', tj 'before'
+                    print line,
+
+            else:
+                print line,  # zarez znaci da se ne ispisuje nova prazna linija poslije komande 'print'(sto python inace radi)
+
+        print 'Text dodan u datoteku %s.' % file_to_add
+        return True
+    except Exception, e:
+        print e
+        return False
+
+
+
+
+def get_text_block_from_template_with_parameters(parameters_dict,template_file_path):
+    template_block_list = []
+    text_block = ''
+    try:
+        f_template = open(template_file_path, 'r').readlines()
+        for line in f_template:
+            for key in parameters_dict.iterkeys():
+                template_block_list.append(line.replace(key, parameters_dict[key]))
+
+        text_block = text_block.join(template_block_list)
+        return text_block
+
+    except Exception, e:
+        print e
+        return False
+
+
+def read_properties_file_parameters(parameter_names_list,properties_file_path):
+    parameters_dict = {}
+    try:
+        if len(parameter_names_list) > 0:
+            prop_f = open(properties_file_path, 'r').readlines()
+            for parameter in parameter_names_list:
+                for line in prop_f:
+                    if parameter in line and line[0] != '#':
+                        value = line.split('=',1)[1].strip()#drugi argument '1' u split() funkciji znaci da radi split samo na prvom znanku '='.to treba jer vrijednosti nekih parametara imaju takoder u sebi znak '=', npr. SCOPE_CONT
+                        parameters_dict[parameter] = value
+        return parameters_dict
+
+    except Exception, e:
+        print e
+        return False
+
+
+
 
